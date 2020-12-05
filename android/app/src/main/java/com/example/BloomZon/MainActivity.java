da8766bee7dbd978cf6942cf0031c0e412d0b27e
daraxdray <40259581+daraxdray@users.noreply.github.com>
2020-12-05 15:46:30 +0100

    BLoomzon MArket Component Completed

---
+++ android/app/src/main/java/com/example/BloomZon/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.BloomZon;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Library/carousel_pro/carousel_pro.dart
@@ -0,0 +1,3 @@
+library carousel_pro;
+
+export 'package:BloomZon/Library/carousel_pro/src/carousel_pro.dart';
